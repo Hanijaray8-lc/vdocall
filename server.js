7ba4a0c3653306aa0687081ca6c15c3b9a9d44b8
Hani☠️ <hanija.lc240073@gmail.com>
2026-04-06 17:23:05 +0530

    Change token endpoint to POST and update payload

--- server.js
+++ server.js
@@ -8,55 +8,62 @@ app.use(express.json());
 
 // 🔥 PUT YOUR VALUES HERE
 const APP_ID = "vpaas-magic-cookie-bef646f17b5d4bd0a4b6d0fb2558b906";  // full app id
-const KID = "6d55ba"; // last part of your API key
+const KID = "52ab6a"; // last part of your API key
 const PRIVATE_KEY = `-----BEGIN PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCC0vHZlU+NNHPT
-PkYli9HtfedutYxt8XqDXDV/o9+uX5W+10PCXONJwXLBPzyfwajFFdbPqPpr7XiH
-vkfNH5viAMY0Uz9oafn4kCsbYh3ZODMpnAUYZMXVLgt7Aji93nrwvQ14vAaHzZDu
-E7IEGUAdtf+zq3Tekdqa1ceRA+3v4qu8iRpEC68dqDD78ZRwcr+6FFPwgDPzidx2
-OQKNPk05Or7+6pRUVQjtf2AjWW1zucZe7oP6INgUdMOgFI64znwn8JRKVkGDnfaL
-CBSnSfEiaM7iKGDxhMzAPzHuBX3di+bLe0b+buVohuAPYX+w3YS8JgkMj9LuzQGZ
-d02lJWu7AgMBAAECggEAf3JLA8IOhqwzrfY71fX5Y9jE7jhU/wJBbNkWkSsvDKu3
-sPv62c0t6ztWtOYLBENgS+ezpgejNiNYyTzYq/8AGFLSxETZP7oGOF0ScZHLLEMw
-rHd/Cz2Y8M3zhzkp1+Z2dDJoflcGMaRUbWqogI+sw4T6/qioqO8EPNTInwBp+se6
-mAwrBJujwF0tjYgQvjQrKE0nshEEhjpHOOXPBUt4wBh2YSoMvAhcUkxBtlNxhrxN
-K2fOBeytWklb7g5U0Vpqq2c4Umly39IelgV/WqOgV35gHdLYMrkAIegP4qanQQGU
-WOBQSWfE/15DV1tWNilTx3ZIvgtL/njU/tLVk4UIAQKBgQDSoDJlSZ10HlLkmR/R
-+6z+JHA37KMpUXF6JKDT/cnebMFXroT+8UucsP+pkjWSTMTjLUWpIftVniF2i9K8
-rZBH1d5PU7oZ9r3MFv3raACwNlH492XMWzCyQhBX5aEJV9A8Dr52NIzXM6IuL1B2
-vyaOuJL/QSmtzY06gu6hQpm/uwKBgQCfAcJgHRV5XEQ8xfsmlu57O3YeOTrWnT2D
-HhHOBKUIyhZtC6DoWfl3fCSIQ7U5hyyfcnPeawYADO5pcTFyASx1EfPYRQE31i9q
-iCsJBUcZuAMgHKcvnMZ9NVZ7Af6oMWGmyjUhXGGw5WVQw/3pDIiSqH5X4+4lcY1k
-TFMoffpEAQKBgQCPzN+bV48wYCOn/wNCkdmiCa6z/9PFud0Hz0oecf8I5Kd7/aK/
-jG5bJFzooPICWHdzwsz0lEbMRZIDJaNTPgxrfxCwiDFe2J7sYCYsOm194RhGKmJO
-C1RL397Yo+Y2QsXguxA+E9K0eoQXlCYTbJ4egtWZgBu8lcH+WLYjUx01UQKBgFIt
-CJW/si9sXd/Cug+xhUKu8lagYSY1UWsICU+d2uT1/C0TC2/JwEHeKMs7otc+YvNs
-pTFXwlwYUsavt0wiCyXdx1YII58F1K+L/CHHVn2WOhdId6cFsRdYj383ArNOAhe+
-/UJmih5wD7Jz+C+PSK3y4ojtxVAYpWgmgfu1T7gBAoGALVBp6j0WYh7nSvUHUybM
-kEWwQzxRngu9Qz/5OlLly18hjD+iDwaGd6hFA+StsED5xpqZHZu4uXdHsOx+AKIx
-h/I6zLYKUZ8qqwfok0FzRls0teEcUXvM/pjoc8gdS4cyUxq5Ai9lIxqFwJw4wMOk
-CIFsg66+S7tEcuvioOOoySE=
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCTQgDuqagsKzSL
+q5aLD5v4BOAIAzCdAwJRFm2lcEozPQ4buSXLX8a0+sp3iF6c2Vw31mHzvd2O3375
+yHdGkjdGsuHlzD0t64JnHu8WUiZSuLDezuRAaOzY3YqNN3pTNPtyGnwj3JtHeVFE
+JlTYEsZbaLSQvz8eHjiA6kA7h1gFLszyMsexHK2mBaCmWN/U4pwWdJPeLMPdhUl2
+kyyrgAW2dQPJGU9sAdbuxb8NbagCC6ieRwYTN3V/orZSgTRUGISMd34hklgTVprC
+nTa4+VdG+01CqTa7ytq11DGJU2QvNzX4Us6aOmm8qKfh8oysmucR8ETr9U1mB8nL
+lyJ+AqV9AgMBAAECggEAfGUraHFFMVqmHr6qg3qCJPfpUJtxO5HJKTqrETBUXghb
+nKvA85ZKw6N/2Y4HbrRnJHwjbsWbHhBKsTLZH3Jw43ZFQdxQajQiin4lNlPbL9H5
+B576TjbUrTYozrZozVVae79H0vuO6MfhEJLY5Vam3Z0Lu6Ly7LxWuwAnlqH0hxsD
+wmSHj6FnciR2JuB18GRBURvtMYVozbgO5XVSSZopg9j9oJ8UkoVbeknZOKNBv76c
+PeHO96vJOitEvJcJ/C+QnCHoZyEe4Oh/+WP1m705nLeJYz94Nqvll2tp6Zgsf5s7
+A4fJ5lTQfMOja1A7eKQqYXf6QfQlOtvkmI6d0w5KuQKBgQDGuTKK86fZSWc3F3w0
+Uqjx0otaez0z4NEGqoXD6uTY6t52CCNsPkJ2U/4A8DupY/bZinSVQKSmTz3KAWGh
+FU9m4w1/viVFufkNXQA/WtxXIyOkGBV593KcT+Re3+T0jOdLnzzfNsOZOufmZb3n
+PVEO4CrnZlVHZOkUeZ4jbLUTFwKBgQC9s2sVCWX2cY6ki9ggFB1zO2f6l/ZZYWKy
+sYvE0Gsk0GK166pew7eQB/fFWmg0tKntYa69GFROQoJbI5OvFgMlD87VddBQdcKm
+WWDIkrHfRahpQepnqghy6anv6yYa9VWu9i52yoqe0W9e4FG6bvKs4KAbAlOERmJJ
+nQQvWEX4iwKBgQCHzfpXcSns2Tf7M0aDL1YbZIotPHlX4bSXPKOLfxQDlYaP3QHw
+MCAVY1L7azEqkpEYm4EOmo2qxg5w5T7F6Z1Ir7QC73vjSbOEyWCo6zwXJIFv27GZ
+l1wwrHxhxdpesYhDasAbvv4u0k22ayM7SvEBHHrVhl6TGsgaYP6MMPcR4wKBgHul
+n611k8U/nUc5c6TtF4kNdqfLNyJpLKjmuwCNDNlRDpYfRsw/67zwxJoZZIS+IWh9
+3CSa4BCmn0sCdHeR9HjKxfkyspZPO5Gr/j6eB1xNjgkVktEA7kLgdQjRj+PQd4n0
+IrMBL7t3LY18zyiiuoDeZN1V9dPpAJWUx66IbgsjAoGAGyC1c+vsf7BGdFhpOLbA
+GM2gvmSScHGjnSkRVwkNX2uwLZx4BcrCsbFdbH69vwJqMU7IYAEnd3NDwqHQp0fg
+KmaxNR0nkgsOF3Va//SIz/cQuQKnQK08KUqPevJkMKe8z+qTlwvGlv9Hn4gTd3VW
+qIzotKi0C/BIFqOMu/nOKfc=
 -----END PRIVATE KEY-----`;
 
 // 🔐 Generate JWT
-app.get("/get-token", (req, res) => {
-  const payload = {
-    aud: "jitsi",
-    iss: "vpaas-magic-cookie-bef646f17b5d4bd0a4b6d0fb2558b906",
-    sub: "8x8.vc",
-    room: "my-room",
-  };
+app.post("/get-token", (req, res) => {
+  const { userName } = req.body;
 
+  const ROOM_NAME = "my-room";
+
+const payload = {
+  aud: "jitsi",
+ iss: APP_ID,
+  sub: "8x8.vc",
+  room: ROOM_NAME, // ✅ EXACT MATCH
+  context: {
+    user: {
+      name: userName || "Guest",
+    },
+  },
+};
   const token = jwt.sign(payload, PRIVATE_KEY, {
     algorithm: "RS256",
-    expiresIn: "1h",
-    header: { kid: KID },
+    expiresIn: "10h",
+    header: {
+      kid: KID   // ✅ IMPORTANT FIX
+    }
   });
 
   res.json({ token });
 });
-
-
-
 
 app.listen(5000, () => console.log("Server running on port 5000"));
